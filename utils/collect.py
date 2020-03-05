c209c71b3fd87b8934f2bf36dd9c3baec9731250
AlexShypula <ags417@stern.nyu.edu>
2020-03-04 23:32:57 -0500

    fix manipulation of the db jsons/dicts based on the structure

--- utils/collect.py
+++ utils/collect.py
@@ -18,30 +18,34 @@ def collect_file_names(database: str, collection: str = "repos") -> Dict[str, Di
 
 	file_names_dictionary = {}
 
-	for compile_result in db_compile_results.find(): 
+	for compile_result in tqdm(db_compile_results.find()): 
 		if compile_result["num_binaries"] > 0: 
-			orig_files = compile_result["binares"]
-			sha256_files = compile_result['sha256']
-			repo_path = "/".join([compile_result["repo_owner"],compile_result["repo_name"]])
-			file2sha = {file: sha for file, sha in zip(orig_files, sha256_files)}
+			for makefile in compile_result["makefiles"]: 
+				# ensure the makefile didn't fail or return no binaries
+				if makefile["success"] == True and makefile["binares"] != [] and makefile["sha256"] != []:  
+					directory = makefile["directory"]
+					orig_files = makefile["binares"]
+					sha256_files = makefile['sha256']
+					repo_path = "/".join([makefile["repo_owner"],compile_result["repo_name"]])
+					file2sha = {file: sha for file, sha in zip(orig_files, sha256_files)}
 
-			for file_name in orig_files: 
-				if file_name[-2:] == ".s": 
-					shared_name = file_name[:-2]
-					if (shared_name + ".o") in orig_files: 
-						ELF = shared_name + ".o"
-					elif shared_name in orig_files: 
-						ELF = shared_name
-					else: 
-						print(f"assembly file: {file_name}'s corresponding ELF could not be found in the following list {orig_files}")
-
-					assembly_path = "/".join([repo_path, file_name])
-					file_names_dictionary[assembly_path] = {"repo_path": repo_path,
-															"assembly_file": file_name,
-															"ELF_file": ELF, 
-															"assembly_sha": file2sha[file_name],
-															"ELF_sha": file2sha[ELF]
-															}
+					for file_name in orig_files: 
+						if file_name[-2:] == ".s": 
+							shared_name = file_name[:-2]
+							if (shared_name + ".o") in orig_files: 
+								ELF = shared_name + ".o"
+							elif shared_name in orig_files: 
+								ELF = shared_name
+							else: 
+								print(f"assembly file: {file_name}'s corresponding ELF could not be found in the following list {orig_files}")
+								continue
+							identifier = "/".join([repo_path, directory, file_name])
+							file_names_dictionary[identifier] = {"repo_path": repo_path,
+																	"assembly_file": file_name,
+																	"ELF_file": ELF, 
+																	"assembly_sha": file2sha[file_name],
+																	"ELF_sha": file2sha[ELF]
+																	}
 	return file_names_dictionary
 
 def read_from_file(file_path: str) -> str: 
@@ -79,7 +83,7 @@ def data_to_csv(out_file_name: str, unopt_compile_path: str, opt_compile_path: s
 	# each key in the dictionary is the concatenation of the repo_path as well as the assembly file name 
 	unoptimized_dictionary = collect_file_names(unopt_db)
 	optimized_dictionary = collect_file_names(opt_db)
-	for assembly_file_name in tqdm(unoptimized_dictionary):
+	for assembly_identifier in tqdm(unoptimized_dictionary):
 
 		if check_for_duplicates: 
 			# skip if the assembly hash has already been processed before
@@ -88,23 +92,24 @@ def data_to_csv(out_file_name: str, unopt_compile_path: str, opt_compile_path: s
 			else: 
 				running_unopt_sha_set.add(unoptimized_dictionary["assembly_sha"])
 
-		if assembly_file_name in optimized_dictionary: 
+		if assembly_identifier in optimized_dictionary: 
 			# get functions for both assembly files as well as corresponding assembly files
-			unopt_fun_list, unopt_assembly_string = functions_and_assembly(unopt_compile_path, unoptimized_dictionary[assembly_file_name])
-			opt_fun_list, opt_assembly_string = functions_and_assembly(opt_compile_path, optimized_dictionary[assembly_file_name])
+			unopt_fun_list, unopt_assembly_string = functions_and_assembly(unopt_compile_path, unoptimized_dictionary[assembly_identifier])
+			opt_fun_list, opt_assembly_string = functions_and_assembly(opt_compile_path, optimized_dictionary[assembly_identifier])
 			# ensure that there is parity between functions
 			if set(unopt_fun_names) == set(opt_fun_names): 
 				# dictionary where keys are function names and values are the assembly 
 				chunk_unopt_assembly = chunk.chunk_assembly(unopt_fun_list, unopt_assembly_string)
 				chunk_opt_assembly = chunk.chunk_assembly(opt_fun_list, opt_assembly_string)
 				for function_name in chunk_unopt_assembly: 
-					csv_row = [assembly_file_name, function_name, chunk_unopt_assembly[function_name], chunk_opt_assembly[function_name]]
+					#TODO: Add the repo path and the assembly hash so you can easily lookup the files for debugging
+					csv_row = [assembly_identifier, function_name, chunk_unopt_assembly[function_name], chunk_opt_assembly[function_name]]
 					chunk.write_to_csv(out_file_name, csv_row)
 			else:
-				print(f"the file {assembly_file_name} had inconsistencies in functions between the unopt and the opt versions\n\n \
+				print(f"the file {assembly_identifier} had inconsistencies in functions between the unopt and the opt versions\n\n \
 							the set of unoptimized functions is {set(unopt_fun_names)}\n \
 							and the set of optimized functions is {set(opt_fun_names)}\n\n")
 		else: 
-			print(f"the file {assembly_file_name} does not exist in the optimized dictionary")
+			print(f"the file {assembly_identifier} does not exist in the optimized dictionary")
 
 
