55c43abe8807a5b10945ab6f87503235d00d2d59
AlexShypula <ags417@stern.nyu.edu>
2020-05-26 18:46:42 -0400

    fix costfn script args

--- demo_cost_fn.sh
+++ demo_cost_fn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 utils/stoke_test_costfn.py -path_list demo_successful_paths.txt -unopt_prefix O0 -opt_prefix Og -stats_out demo_costfn/demo_stats.csv -tc_gen_log demo_costfn/demo_tc_gen.log -cost_log demo_costfn/demo_cost.log -benchmark_log demo_costfn/demo_benchmark.log -n_workers 1
+python3 utils/stoke_test_costfn.py -path_list demo_successful_paths.txt --unopt-prefix O0 --opt-prefix Og -stats_out demo_costfn/demo_stats.csv -tc_gen_log demo_costfn/demo_tc_gen.log -cost_log demo_costfn/demo_cost.log -benchmark_log demo_costfn/demo_benchmark.log --n-workers 1
