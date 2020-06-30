e0a32a3670c1db4939e4689e0941293bc408ec86
AlexShypula <ags417@stern.nyu.edu>
2020-06-30 18:26:38 -0400

    adding edits to run cost fn and gather stats and also create data for training

--- utils/preprocess_hackers.py
+++ utils/preprocess_hackers.py
@@ -1,10 +1,14 @@
 import subprocess
+import csv
 from typing import List
 import os
 from os import makedirs
 from os.path import splitext, join
 from dataclasses import dataclass, field
 from argparse_dataclass import ArgumentParser
+from stoke_preprocess import hash_file, process_raw_assembly
+from stoke_test_costfn import test_costfn
+import sentencepiece as spm
 
 
 @dataclass
@@ -18,33 +22,125 @@ def mkdir(dir:str):
     if not os.path.exists(dir):
         makedirs(dir)
 
+def merge_registers(a):
+    '''
+    merge back split registers
+    '''
+    res = []
+    last = a[0]
+    for current in a[1:]:
+        if last == None:
+            last = current
+            continue
+        if last != '▁%' and last != '%':
+            res.append(last)
+            last = current
+        else:
+            res.append(''.join([last, current]))
+            last = None
+    return res
+
 
 def pre_process(c_progs: List[str], asbly_prefixes: List[str],
                 destination_dir: str = "processed_data",
+                csv_out: str = "cost_stats.csv",
                 compilation_flags: List[str] = ["O0", "Og", "O2", "O3"]):
-    for c_prog_pth, asbly_prefix in zip(c_progs, asbly_prefixes):
-        process_program(file_path=c_prog_pth,
+
+    stats_csv_fh = open(join(destination_dir, csv_out), "w")
+    field_names = ["problem"]
+    for compilation_flag in compilation_flags:
+        field_names.append(f"{compilation_flag}-O0_cost")
+        field_names.append(f"{compilation_flag}-O0_correct")
+    dict_writer = csv.DictWriter(stats_csv_fh,
+                                 fieldnames=field_names,
+                                 delimiter=",",
+                                 quoting=csv.QUOTE_ALL)
+    dict_writer.writeheader()
+    for i, (c_prog_pth, asbly_prefix) in enumerate(zip(c_progs, asbly_prefixes)):
+        cost_dict = process_program(file_path=c_prog_pth,
                         assembly_file_name = asbly_prefix,
+                        problem_no = i,
                         destination_dir=destination_dir,
                         compilation_flags = compilation_flags)
+        dict_writer.writerow(cost_dict)
+
+    stats_csv_fh.close()
+
+
+def make_data(c_progs: List[str],
+              asbly_prefixes: List[str],
+              spm_model_path: str,
+              destination_dir: str = "processed_data",
+              data_dir = "model_data",
+              src_flag = "O0",
+              tgt_flag = "Og"
+              ):
+    model_data_dir = join(destination_dir, data_dir)
+    mkdir(model_data_dir)
+    train_src = open(join(model_data_dir, "train.src"), "w")
+    train_tgt = open(join(model_data_dir, "train.tgt"), "w")
+    val_src = open(join(model_data_dir, "val.src"), "w")
+    val_tgt = open(join(model_data_dir, "val.tgt"), "w")
+    test_src = open(join(model_data_dir, "test.src"), "w")
+    test_tgt = open(join(model_data_dir, "test.tgt"), "w")
+
+    sent_piece = spm.SentencePieceProcessor()
+    sent_piece.Load(spm_model_path)
+
+    for i, (c_prog_pth, asbly_prefix) in enumerate(zip(c_progs, asbly_prefixes)):
+        program_directory = join(destination_dir, os.path.basename(os.path.dirname(c_prog_pth)))
+        for compilation_flag in (src_flag, tgt_flag):
+            function_dir = join(program_directory, compilation_flag, "functions")
+            asbly_path = join(function_dir, asbly_prefix + ".s")
+            with open(asbly_path, "r") as f:
+                raw_asbly = f.read()
+                processed_asbly, _, _, _ = process_raw_assembly(raw_asbly)
+                tokenized_asbly = merge_registers(sent_piece.EncodeAsPieces(processed_asbly.strip()))
+                asbly_str = " ".join(tokenized_asbly)
+
+            if compilation_flag == "O0":
+                if i < 15:
+                    train_src.write(asbly_str+'\n')
+                elif i < 20:
+                    val_src.write(asbly_str+'\n')
+                else:
+                    test_src.write(asbly_str+'\n')
+            else:
+                if i < 15:
+                    train_tgt.write(asbly_str+'\n')
+                elif i < 20:
+                    val_tgt.write(asbly_str+'\n')
+                else:
+                    test_tgt.write(asbly_str+'\n')
+
+    train_src.close()
+    train_tgt.close()
+    val_src.close()
+    val_tgt.close()
+    test_src.close()
 
 
 def process_program(file_path: str,
                     assembly_file_name: str,
+                    problem_no: int,
                     destination_dir: str = "processed_data",
-                    compilation_flags: List[str] = ["O0", "Og", "O2", "O3"],
+                    compilation_flags: List[str] = ["O0", "Og", "O2", "O3"]
                     ):
+    assert compilation_flags[0] == "O0", "first compilation flag must be O0 for the purpose of making comparisons"
     #breakpoint()
-    file_prefix = splitext(file_path)[0]
+    #file_prefix = splitext(file_path)[0]
     #program_directory = join(destination_dir, file_prefix)
-    program_directory = join(destination_dir, os.path.basename(os.path.dirname(file_path)))
+    #model_data_dir = join(destination_dir, "model_data")
+    problem_no = os.path.basename(os.path.dirname(file_path))
+    program_directory = join(destination_dir, problem_no)
     testcases_directory = join(program_directory, "testcases")
     ### make directories
     mkdir(testcases_directory)
+    cost_dict = {"problem": problem_no}
     for compilation_flag in compilation_flags:
         for subfolder in ("functions", "bin"):
             mkdir(join(program_directory, compilation_flag, subfolder))
-        fxn_file =  join(program_directory, compilation_flag, "bin", "fxn.o")
+        fxn_file = join(program_directory, compilation_flag, "bin", "fxn.o")
         bin_file = join(program_directory, compilation_flag, "bin", "a.out")
         fxn_compile = subprocess.run(["g++", "-std=c++11",f"-{compilation_flag}", "-c", os.path.dirname(file_path) + "/fxn.cc", "-fno-inline", "-o", fxn_file])
         compilation = subprocess.run(["g++",
@@ -57,7 +153,7 @@ def process_program(file_path: str,
                                          text=True,
                                          timeout=300
                                      )
-        if compilation.returncode != 0:
+        if compilation.returncode != 0 or fxn_compile != 0:
             print(f"compilation on {file_path} at {compilation_flag} flag failed: {compilation.stdout}")
             continue
         else:
@@ -78,10 +174,10 @@ def process_program(file_path: str,
             else:
                 print(f"stoke extract worked for {file_path} at {compilation_flag}")
                 if compilation_flag == "O0":
-                    asbly_path = join(function_dir, assembly_file_name + ".s")
+                    base_asbly_path = join(function_dir, assembly_file_name + ".s")
                     tc_path = join(testcases_directory, assembly_file_name + ".tc")
                     tc = subprocess.run(['stoke', 'testcase',
-                                    '--target', asbly_path,
+                                    '--target', base_asbly_path,
                                     "-o", tc_path,
                                     '--functions', function_dir,
                                     "--prune",
@@ -92,10 +188,26 @@ def process_program(file_path: str,
                                    text=True,
                                    timeout=300)
                     if tc.returncode != 0:
+                        successful_testcase_flag = False
                         print(f"testcases on {file_path} at {compilation_flag} flag failed: {tc.stdout}")
                         continue
                     else:
+                        successful_testcase_flag = True
                         print(f"stoke testcase generation worked for {file_path} at {compilation_flag}")
+
+                if successful_testcase_flag:
+                    test_asbly_path = join(function_dir, assembly_file_name + ".s")
+                    rc, stdout, cost, correct  = test_costfn(target_f = base_asbly_path,
+                                                             rewrite_f = test_asbly_path,
+                                                             testcases_f = tc_path,
+                                                             fun_dir = function_dir,
+                                                             live_dangerously = True)
+                    cost_dict[f"{compilation_flag}-O0_cost"] = float(cost)
+                    cost_dict[f"{compilation_flag}-O0_correct"] = True if correct == "yes" else False
+
+    return cost_dict
+
+
 
 if __name__ == "__main__":
     #breakpoint()
