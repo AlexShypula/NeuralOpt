d0d0ab642b2027139a6d3eadfad10e7ffb93b69a
AlexShypula <ags417@stern.nyu.edu>
2020-07-01 11:10:07 -0400

    add functionality to extract from hackers

--- utils/preprocess_hackers.py
+++ utils/preprocess_hackers.py
@@ -1,14 +1,76 @@
 import subprocess
 import csv
-from typing import List
+import re
 import os
+import sentencepiece as spm
+from typing import List, Dict
 from os import makedirs
 from os.path import splitext, join
 from dataclasses import dataclass, field
 from argparse_dataclass import ArgumentParser
 from stoke_preprocess import hash_file, process_raw_assembly
-from stoke_test_costfn import test_costfn
-import sentencepiece as spm
+from stoke_test_costfn import COST_SEARCH_REGEX, CORRECT_SEARCH_REGEX
+
+
+DEF_IN_REGEX = re.compile('(?<=--def_in\s)"[^\n#]+')
+LIVE_OUT_REGEX = re.compile('(?<=--live_out\s)"[^\n#]+')
+DISTANCE_REGEX = re.compile('(?<=--distance\s)[^\n#]')
+MISALIGN_PENALTY_REGEX = re.compile('(?<=--misalign_penaltyt\s)[^\n#]')
+SIG_PENALTY_REGEX = re.compile('(?<=--sig_penaltyt\s)[^\n#]')
+COSTFN_REGEX = re.compile('(?<=--cost\s)"[^\n#]+')
+
+
+def _search_and_strip(string: str, pattern: re.Pattern):
+    return pattern.search(string).group().strip()
+
+
+def extract_conf(conf_path: str):
+    with open(conf_path, "r") as fh:
+        conf = fh.read()
+    conf_dict = {"def_in": _search_and_strip(conf, DEF_IN_REGEX),
+                 "live_out": _search_and_strip(conf, LIVE_OUT_REGEX),
+                 "distance": _search_and_strip(conf, DISTANCE_REGEX),
+                 "misalign_penalty": _search_and_strip(conf, MISALIGN_PENALTY_REGEX).
+                 "sig_penalty": _search_and_strip(conf, SIG_PENALTY_REGEX),
+                 "costfn": _search_and_strip(conf, COSTFN_REGEX)}
+    return conf_dict
+
+
+def test_costfn_hackers(target_f: str,
+                rewrite_f: str,
+                testcases_f: str,
+                fun_dir: str,
+                settings_conf: Dict[str, str],
+                live_dangerously = True):
+	live_dangerously_str = "--live_dangerously" if live_dangerously else ""
+	try:
+		cost_test = subprocess.run(
+			['stoke', 'debug', 'cost',
+             '--target', target_f,
+             '--rewrite', rewrite_f,
+             '--testcases', testcases_f,
+             '--functions', fun_dir,
+             "--prune", live_dangerously_str,
+             "--def_in", settings_conf["def_in"],
+             "--live_out", settings_conf["live_out"]
+             "--distance", settings_conf["distance"]
+             "--misalign_penalty", settings_conf["misalign_penalty"]
+             "--sig_penalty", settings_conf["sig_penalty"]
+             "--cost", settings_conf["costfn"]],
+			stdout=subprocess.PIPE,
+			stderr=subprocess.STDOUT,
+			text=True,
+			timeout=25)
+		if cost_test.returncode == 0:
+			cost = COST_SEARCH_REGEX.search(cost_test.stdout).group()
+			correct = CORRECT_SEARCH_REGEX.search(cost_test.stdout).group()
+		else:
+			cost = -10701
+			correct = "failed"
+		return cost_test.returncode, cost_test.stdout, cost, correct
+
+	except subprocess.TimeoutExpired as err:
+		return -11785, err, -11785, "timeout"
 
 
 @dataclass
@@ -52,6 +114,7 @@ def pre_process(c_progs: List[str], asbly_prefixes: List[str], spm_model_path: s
     for compilation_flag in compilation_flags:
         field_names.append(f"{compilation_flag}-O0_cost")
         field_names.append(f"{compilation_flag}-O0_correct")
+    field_names.extend(["def_in", "live_out", "distance", "misalign_penalty", "sig_penalty", "costfn"])
     dict_writer = csv.DictWriter(stats_csv_fh,
                                  fieldnames=field_names,
                                  delimiter=",",
@@ -130,16 +193,17 @@ def process_program(file_path: str,
                     compilation_flags: List[str] = ["O0", "Og", "O2", "O3"]
                     ):
     assert compilation_flags[0] == "O0", "first compilation flag must be O0 for the purpose of making comparisons"
-    #breakpoint()
-    #file_prefix = splitext(file_path)[0]
-    #program_directory = join(destination_dir, file_prefix)
-    #model_data_dir = join(destination_dir, "model_data")
     problem_no = os.path.basename(os.path.dirname(file_path))
     program_directory = join(destination_dir, problem_no)
     testcases_directory = join(program_directory, "testcases")
     ### make directories
     mkdir(testcases_directory)
     cost_dict = {"problem": problem_no}
+
+    cost_conf_path = os.path.dirname(file_path) + "/opt.conf"
+    cost_conf_dict = extract_conf(cost_conf_path)
+    cost_dict.update(cost_conf_dict)
+
     for compilation_flag in compilation_flags:
         for subfolder in ("functions", "bin"):
             mkdir(join(program_directory, compilation_flag, subfolder))
@@ -200,16 +264,16 @@ def process_program(file_path: str,
 
                 if successful_testcase_flag:
                     test_asbly_path = join(function_dir, assembly_file_name + ".s")
-                    rc, stdout, cost, correct  = test_costfn(target_f = base_asbly_path,
-                                                             rewrite_f = test_asbly_path,
-                                                             testcases_f = tc_path,
-                                                             fun_dir = function_dir,
-                                                             live_dangerously = True)
+                    rc, stdout, cost, correct  = test_costfn_hackers(target_f = base_asbly_path,
+                                                                        rewrite_f = test_asbly_path,
+                                                                        testcases_f = tc_path,
+                                                                        fun_dir = function_dir,
+                                                                        settings_conf=cost_conf_dict,
+                                                                        live_dangerously = True)
                     cost_dict[f"{compilation_flag}-O0_cost"] = float(cost)
                     cost_dict[f"{compilation_flag}-O0_correct"] = True if correct == "yes" else False
 
     return cost_dict
-
 
 
 if __name__ == "__main__":
