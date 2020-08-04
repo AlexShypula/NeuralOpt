6e2053c8bfd4a3492fffdc760f5cc6f1f24be694
AlexShypula <ags417@stern.nyu.edu>
2020-08-04 18:52:42 -0400

    add deduplicate util

---
+++ utils/deduplicate.py
@@ -0,0 +1,34 @@
+from stoke_preprocess import hashfile
+from dataclasses import dataclass, field
+from argparse_dataclass import ArgumentParser
+from tqdm import tqdm
+
+@dataclass
+class ParseOptions:
+    source: str = field(metadata=dict(args=["-src", "--source"]))
+    target: str = field(metadata=dict(args=["-tgt", "--target"]))
+    source_rewrite: str = field(metadata=dict(args=["-src_out", "--source_output"]))
+    target_rewrite: str = field(metadata=dict(args=["-tgt_out", "--target_output"]))
+
+if __name__ == "__main__":
+    parser = ArgumentParser(ParseOptions)
+    print(parser.parse_args())
+    args = parser.parse_args()
+    sha_set = set()
+    src_final = open(args.source_rewrite, "w")
+    tgt_final = open(args.target_rewrite, "w")
+    dups = 0
+    with open(args.source) as src, open(args.target) as tgt:
+        src_lines = src.readlines()
+        tgt_lines = tgt.readlines()
+        for s, t in tqdm(zip(src_lines, tgt_lines)):
+            sha = hashfile(s.strip().lower())
+            if sha not in sha_set:
+                src_final.write(s + "\n")
+                tgt_final.write(t + "\n")
+                sha_set.add(sha)
+            else:
+                dups += 1
+    print(f"total number of lines was {len(src_lines)} and dups were {dups}")
+    src_final.close()
+    tgt_final.close()
