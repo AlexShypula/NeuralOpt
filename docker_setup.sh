1fe6215a7d68f4979863a9eac2b23936da7c8d41
AlexShypula <ags417@stern.nyu.edu>
2020-08-03 23:02:24 -0400

    update docker setup to publish new port for api

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo docker pull stanfordpl/stoke:latest
-sudo docker run -d -P --name TorchStoke -v $(pwd)/docker:/home/stoke/docker -it stanfordpl/stoke:latest
+sudo docker run -d -P --name TorchStoke -p 6000:6000 -v $(pwd)/docker:/home/stoke/docker -it stanfordpl/stoke:latest
+#sudo docker run -d -P --name TorchStoke2 -p 32770:22 -p 6000:6000 -v $(pwd)/docker:/home/stoke/docker -it torch_stoke_base 
 sudo docker port TorchStoke 22
