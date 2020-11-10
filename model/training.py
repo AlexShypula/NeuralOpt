fix bug in computing the average cost when below max cost

model/training.py
@@ -1037,7 +1037,7 @@ class TrainManager:
                     corrects_arr = np.array(multi_batch_corrects)
                     avg_not_failed_cost = sum(np.multiply(not_failed_arr, costs_arr))/sum(not_failed_arr)
                     avg_not_failed_correct = sum(np.multiply(not_failed_arr, corrects_arr))/sum(not_failed_arr)
-                    avg_cost_below_max_cost = np.mean(np.multiply((costs_arr<self.max_score), costs_arr))
+                    avg_cost_below_max_cost = sum(np.multiply((costs_arr<self.max_score), costs_arr)) / sum((costs_arr<self.max_score))
                     self.tb_writer.add_scalar("train/avg_not_failed_cost", avg_not_failed_cost, update_no)
                     self.tb_writer.add_scalar("train/pct_correct_when_not_failed", avg_not_failed_correct, update_no)
                     self.tb_writer.add_scalar("train/avg_cost_below_max_cost", avg_cost_below_max_cost, update_no)