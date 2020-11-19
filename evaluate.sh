7e5269e0cdd9fac82c7c2707d158d8c277b15750
AlexShypula <ags417@stern.nyu.edu>
2020-11-19 18:20:19 -0500

    switch evaluate.sh to a for loop over checkpoints to evaluate

--- evaluate.sh
+++ evaluate.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+EXPERIMENT_NAME="EXPERIMENT_NAME_HERE"
+DATASETS_TO_TEST="dev:train"
 
-python3 model/predict.py --path_to_config insert_path_to_config_here \
-  --path_to_checkpoint insert_ckpt_path_here \
+for CHECKPOINT in path_to_ckpt1 path_to_ckpt2 path_to_ckpt3
+do
+  python3 model/predict.py --path_to_config insert_path_to_config_here \
+  --path_to_checkpoint $CHECKPOINT \
   --nbest_beams 10 \
   --beam_size 10 \
   --api_ip_address 127.0.0.1 \
   --api_port_no 6001 \
-  --datasets_to_test dev:train \
-  --experiment_name insert_experiment_name_here
-  ## --output_path ## in case you want to save it elsewhere besides model folder
+  --datasets_to_test $DATASETS_TO_TEST \
+  --experiment_name $EXPERIMENT_NAME
+done
+
+#### an additional argument we can specify in the args is here:
+## --output_path ## in case you want to save it elsewhere besides model folder
