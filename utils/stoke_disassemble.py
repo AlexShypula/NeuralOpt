9bf4469225384c69ffea6873474254b1eba29491
AlexShypula <ags417@stern.nyu.edu>
2020-12-05 17:33:09 -0500

    add executable and make sure not duplicate binary tests in stoke_disassemble

--- utils/stoke_disassemble.py
+++ utils/stoke_disassemble.py
@@ -12,6 +12,18 @@ from dataclasses import dataclass, field
 from argparse_dataclass import ArgumentParser
 import re
 
+EXECUTABLE_FILE_TAG = b"executable"
+
+def _check_executable_fn(path: str) -> (bool, str):
+    r"""Checks whether the specified file is an executable binary file.
+    :param patj: The path to the binary file to test
+    :return: If ``True``, the file is a binary (ELF) file.
+    """
+    output = subprocess.check_output(["file", path], timeout=10)
+    output = output[len(path):]  # first part is file name
+    return EXECUTABLE_FILE_TAG in output, output
+
+
 COLLAPSE_PATTERN = re.compile("/\./")
 
 
@@ -28,10 +40,12 @@ class ParseOptions:
     successful_path_out_file: str = field(metadata=dict(args=["-successful_path_out", "--successful_path_out_file"]))
     n_workers: int = field(metadata=dict(args=["-n_workers", "--n_workers"]), default=1)
     debug: bool = field(metadata=dict(args=["-debug", "--debug_mode"]), default=False)
+    path_to_alt_bin_dir: str = field(metadata=dict(args=["-path_to_alt_bin_dir", "--abs_path_to_alt_bin_dir"]),
+                                     default=None)
 
 
 def load_and_disassemle(binary_info_in_file: str, path_to_bin_dir: str, path_to_disas_dir: str, optimization_flag: str,
-                      successful_path_out_file: str, n_workers=1, debug=False) -> None:
+                      successful_path_out_file: str, n_workers=1, path_to_alt_bin_dir: str= None, debug=False) -> None:
 
     assert not os.path.exists(
         successful_path_out_file), "the out-file specified already exists, please change or delete"
@@ -41,7 +55,7 @@ def load_and_disassemle(binary_info_in_file: str, path_to_bin_dir: str, path_to_
 
     successful_paths = parallel_disassemble(binary_info_dict=binary_info_dict, path_to_bin_dir=path_to_bin_dir,
                                             path_to_disas_dir=path_to_disas_dir, optimization_flag=optimization_flag,
-                                            n_workers=n_workers, debug=debug)
+                                            n_workers=n_workers, path_to_alt_bin_dir=path_to_alt_bin_dir, debug=debug)
 
     with open(successful_path_out_file, "w") as fh:
         for p in successful_paths: 
@@ -51,7 +65,7 @@ def load_and_disassemle(binary_info_in_file: str, path_to_bin_dir: str, path_to_
 
 
 def parallel_disassemble(binary_info_dict: Dict[str, Dict], path_to_bin_dir: str,  path_to_disas_dir: str,
-                         optimization_flag: str, n_workers=1, debug=False):
+                         optimization_flag: str, n_workers=1, path_to_alt_bin_dir: str = None, debug=False):
     running_hash_set = set()
     jobs_list = []
     for i, (binary_identifier, binary_dictionary) in enumerate(tqdm(binary_info_dict.items())):
@@ -70,7 +84,9 @@ def parallel_disassemble(binary_info_dict: Dict[str, Dict], path_to_bin_dir: str
                                    "path_to_bin_dir": path_to_bin_dir,
                                    "rel_path_to_bin_identifier": rel_path_to_bin_identifier,
                                    "optimization_prefix": optimization_flag,
-                                   "binary_dictionary": binary_dictionary}
+                                   "binary_dictionary": binary_dictionary,
+                                   "path_to_alt_bin_dir": path_to_alt_bin_dir
+                                   }
             jobs_list.append(copy_and_disas_dict)
 
     #jobs_list = jobs_list[:3000]
@@ -104,7 +120,7 @@ def copy_and_disas_wrapper(args):
 
 
 def copy_and_disas(path_to_disas_dir: str, path_to_bin_dir: str, rel_path_to_bin_identifier: str,
-                   optimization_prefix: str, binary_dictionary: str) -> (str, bool, str):
+                   optimization_prefix: str, binary_dictionary: str, path_to_alt_bin_dir: str) -> (str, bool, str):
     try:
         lcl_bin_fldr = os.path.join(
             path_to_disas_dir, rel_path_to_bin_identifier, optimization_prefix, "bin")
@@ -120,9 +136,28 @@ def copy_and_disas(path_to_disas_dir: str, path_to_bin_dir: str, rel_path_to_bin
 
     path_to_orig_bin = os.path.join(
         path_to_bin_dir, binary_dictionary["repo_path"], binary_dictionary["file_hash"])
+
+    # check if it exists
     if not os.path.exists(path_to_orig_bin):
         err = f"binary file: {path_to_orig_bin} does not exist"
         return rel_path_to_bin_identifier, False, err
+
+    # tests if -O0 and -Og binaries are the same (as evidenced by the same file ending / hash of the binary)
+    if path_to_alt_bin_dir:
+        path_to_alt_bin = os.path.join(path_to_alt_bin_dir,
+            binary_dictionary["repo_path"], binary_dictionary["file_hash"])
+        if os.path.exists(path_to_alt_bin):
+            err = f"binary file: {path_to_orig_bin} has duplicate in alternative directory {path_to_alt_bin_dir}" \
+                  f"where path is {path_to_alt_bin}"
+            return rel_path_to_bin_identifier, False, err
+
+    # check if executable
+    is_executable, linux_file_cmd_output = _check_executable_fn(path_to_orig_bin)
+    if not is_executable:
+        err = f"binary file: {path_to_orig_bin} is not executable, w/output {linux_file_cmd_output}"
+        return rel_path_to_bin_identifier, False, err
+
+    # othherwise, it exists, is executable, and also is not identical to the counterpart
     path_to_local_bin = os.path.join(
         lcl_bin_fldr, binary_dictionary["file_hash"])
     # omit copying the binary
