67f95f261504a83e98c1722d81b41f43001774ad
AlexShypula <ags417@stern.nyu.edu>
2021-01-07 15:21:49 -0500

    add new shell script for redefine_live_out

--- redefine_live_out.sh
+++ redefine_live_out.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
+PATH_TO_DISASSEMBLY_DIR=""
+PATH_TO_IN_STATS_DF=""
+PATH_TO_OUT_STATS_DF=""
+PATH_TO_SPURIOUS_PROGS=""
+SPURIOUS_PROG_LIST="eax_zero.s:rax_zero.s:retq.s"
+OPTIMIZED_FLAG="Og"
+N_WORKERS="8"
+TIMEOUT="120"
+
 python3 utils/redefine_live_out.py \
-  --path_to_disassembly_dir ../ \
-  --path_to_in_stats_df ../docker/8_17_costfn/stats_200k.csv \
-  --path_to_out_stats_df ../docker/8_17_costfn/stats_redefined.csv \
-  --optimized_flag "Og" \
-  --debug
+  --path_to_disassembly_dir $PATH_TO_DISASSEMBLY_DIR \
+  --path_to_in_stats_df $PATH_TO_IN_STATS_DF \
+  --path_to_out_stats_df $PATH_TO_OUT_STATS_DF \
+  --path_to_spurious_dir $PATH_TO_SPURIOUS_PROGS \
+  --spurious_prog_list $SPURIOUS_PROG_LIST \
+  --optimized_flag $OPTIMIZED_FLAG \
+  --n_workers $N_WORKERS \
+  --timeout $TIMEOUT
+# --debug
